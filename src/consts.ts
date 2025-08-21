f72cb40eae09913e56ea4284b46b177f27d5c99e
Rowan <malice.peanutbutter@gmail.com>
2025-08-22 00:45:26 +0200

    Updated consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'typesomewords';
-export const SITE_DESCRIPTION = 'A minimal blog for thoughts and ideas';
+export const SITE_TITLE = 'Type Some Words';
+export const SITE_DESCRIPTION = 'Rowan typing some words about things.';
